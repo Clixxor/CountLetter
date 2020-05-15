7d40737e1e5d50075ca0af03ab4e5386d6314597
Nico Scheuvens <nico@scheuvens-mg.de>
2020-05-15 11:06:31 +0200

    Projektdateien hinzufügen.

---
+++ CountLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CountLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welches Wort wollen Sie überprüfen?");
+            string Wort = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Welchen Buchstaben suchen Sie?");
+            char Buchstabe = Convert.ToChar(Console.ReadLine().ToLower());
+
+            int Zähler = 0;
+            char charToCount = Buchstabe;
+
+            foreach (char c in Wort)
+            {
+                if (c == charToCount)
+                {
+                    Zähler++;
+                }
+            }
+
+            Console.WriteLine("Die Anzahl des Buchstaben: " + Buchstabe + " ist: " + Zähler);
+        }
+    }
+}
